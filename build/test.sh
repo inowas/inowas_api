e6afe7679b5b91b03f0b9d4f8d00b667539ffdfe
Ralf Junghanns <ralf.junghanns@gmail.com>
2015-11-03 13:31:12 +0100

    Add database for tests in test environment

--- build/test.sh
+++ build/test.sh
@@ -1,9 +1,24 @@
 #!/usr/bin/env bash
 
+/etc/init.d/postgresql stop
+/etc/init.d/postgresql start
+
+DIR=/var/www/html
+SQL_DIR=$DIR/build/sql
+
+cd $DIR
+app/console doctrine:database:drop --force --env=test
+app/console doctrine:database:create --env=test
+
+su - postgres -c "psql inowas_entities_test < "$SQL_DIR"/structure.sql"
+
+app/console doctrine:schema:create --env=test
+
+su - postgres -c "psql inowas_entities_test < "$SQL_DIR"/raster.sql"
+
 DIR=/var/www/html
 SQL_DIR=$DIR/build/sql
 
 cd $DIR
 
-./build/build.sh
 phpunit -c .
