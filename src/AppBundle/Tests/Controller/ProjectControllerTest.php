2c2c65019d2cd8a95fe13fcf008c3e84ad756c77
Ralf Junghanns <ralf.junghanns@gmail.com>
2015-11-25 15:25:22 +0100

    Adapt PorjectControllerTest

--- src/AppBundle/Tests/Controller/ProjectControllerTest.php
+++ src/AppBundle/Tests/Controller/ProjectControllerTest.php
@@ -5,6 +5,7 @@ namespace AppBundle\Tests\Controller;
 use AppBundle\Entity\Project;
 use AppBundle\Entity\User;
 use AppBundle\Model\ProjectFactory;
+use JMS\Serializer\Serializer;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class ProjectControllerTest extends WebTestCase
@@ -12,6 +13,9 @@ class ProjectControllerTest extends WebTestCase
 
     /** @var \Doctrine\ORM\EntityManager */
     protected $entityManager;
+
+    /** @var Serializer */
+    protected $serializer;
 
     /** @var User $user */
     protected $user;
@@ -31,6 +35,9 @@ class ProjectControllerTest extends WebTestCase
         $this->entityManager = static::$kernel->getContainer()
             ->get('doctrine.orm.default_entity_manager')
         ;
+
+        $this->serializer = static::$kernel->getContainer()
+            ->get('jms_serializer');
 
         // Setup
         $this->user = new User();
@@ -56,7 +63,12 @@ class ProjectControllerTest extends WebTestCase
         $client = static::createClient();
         $client->request('GET', '/api/users/'.$this->user->getUsername().'/projects.json');
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
-        $this->assertTrue(true);
+
+        /** @var Project[] $projectArray */
+        $projectArray = $this->serializer->deserialize($client->getResponse()->getContent(), 'array<AppBundle\Entity\Project>', 'json');
+        $this->assertCount(1, $projectArray);
+        $project = $projectArray[0];
+        $this->assertEquals($this->projectname, $project->getName());
     }
 
     /**
