79f42579e07ccd773a16394a544f08580634cad0
Ralf Junghanns <ralf.junghanns@gmail.com>
2016-02-12 15:23:15 +0100

    Adapt test-script

--- build/test.sh
+++ build/test.sh
@@ -10,15 +10,15 @@ cd $DIR
 bin/console doctrine:database:drop --force --env=test
 bin/console doctrine:database:create --env=test
 
-su - postgres -c "psql inowas_entities_test < "$SQL_DIR"/structure.sql"
+su - postgres -c "psql inowas_test < "$SQL_DIR"/structure.sql"
 
 bin/console doctrine:schema:create --env=test
 
-#su - postgres -c "psql inowas_entities_test < "$SQL_DIR"/raster.sql"
+#su - postgres -c "psql inowas_test < "$SQL_DIR"/raster.sql"
 
 #DIR=/var/www/html
 #SQL_DIR=$DIR/build/sql
 
 cd $DIR
 
-phpunit -c .
+phpunit
