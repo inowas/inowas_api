8b64fb1f307481dc4dc1165f01005be21f86c987
Ralf Junghanns <ralf.junghanns@adensis.com>
2016-05-18 10:52:26 +0200

    Adapt build-scripts to server

--- build/sql/structure.sql
+++ build/sql/structure.sql
@@ -2,5 +2,3 @@ CREATE LANGUAGE plpython2u;
 
 CREATE EXTENSION postgis;
 CREATE EXTENSION postgis_topology;
-
-CREATE DOMAIN simple_raster AS FLOAT[][];
--- build/test.sh
+++ build/test.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-#/etc/init.d/postgresql stop
-#/etc/init.d/postgresql start
+/etc/init.d/postgresql stop
+/etc/init.d/postgresql start
 
 DIR=.
 SQL_DIR=$DIR/build/sql
@@ -9,16 +9,7 @@ SQL_DIR=$DIR/build/sql
 cd $DIR
 bin/console doctrine:database:drop --force --env=test
 bin/console doctrine:database:create --env=test
-
-psql inowas_test < "$SQL_DIR"/structure.sql
-
+su postgres -c "psql inowas_dev_test < "$SQL_DIR"/structure.sql"
 bin/console doctrine:schema:create --env=test
-
-#su - postgres -c "psql inowas_test < "$SQL_DIR"/raster.sql"
-
-#DIR=/var/www/html
-#SQL_DIR=$DIR/build/sql
-
 cd $DIR
-
-phpunit
+./vendor/bin/phpunit
