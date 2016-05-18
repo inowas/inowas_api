b10854e8ec875bc468afed0488f323391b4c1f97
Ralf Junghanns <ralf.junghanns@gmail.com>
2016-05-18 12:46:35 +0200

    Adapt loadfixtures-script

--- build/loadFixtures.sh
+++ build/loadFixtures.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-./build/build.sh
 bin/console doctrine:fixtures:load -n --fixtures=src/AppBundle/DataFixtures/ORM/Scenarios/PropertyTypes
 bin/console doctrine:fixtures:load --fixtures=src/AppBundle/DataFixtures/ORM/Scenarios/Scenario_1 --append
 bin/console doctrine:fixtures:load --fixtures=src/AppBundle/DataFixtures/ORM/Scenarios/Scenario_2 --append
