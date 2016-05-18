52fb6e3322c5031e75b1191e27d71ce83ed3bae4
Ralf Junghanns <ralf.junghanns@gmail.com>
2016-05-18 12:19:14 +0200

    Adapt build/test.osx.sh

--- build/test.osx.sh
+++ build/test.osx.sh
@@ -5,9 +5,8 @@ SQL_DIR=$DIR/build/sql
 
 cd $DIR
 bin/console doctrine:database:drop --force --env=test
-DBNAME=$(bin/console doctrine:database:create --env=test | grep -Po '".*?"')
-
-su postgres -c "psql $DBNAME < "$SQL_DIR"/structure.sql"
+bin/console doctrine:database:create --env=test
+psql inowas_test < "$SQL_DIR"/structure.sql
 bin/console doctrine:schema:create --env=test
 cd $DIR
 ./vendor/bin/phpunit
