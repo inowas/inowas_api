c605acae995b4de09aa6a9d3bab49457fc1ea1fa
Ralf Junghanns <ralf.junghanns@gmail.com>
2016-06-22 14:13:23 +0200

    Update loadfixtures-scripts

---
+++ build/loadFixtures.debian.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+./build.debian.sh
+
+bin/console doctrine:fixtures:load --fixtures=src/AppBundle/DataFixtures/ORM/Scenarios/PropertyTypes --append
+bin/console doctrine:fixtures:load --fixtures=src/AppBundle/DataFixtures/ORM/TestScenarios/Scenario_1_Lake_Example --append
+bin/console doctrine:fixtures:load --fixtures=src/AppBundle/DataFixtures/ORM/Scenarios/Scenario_1/ --append
+bin/console doctrine:fixtures:load --fixtures=src/AppBundle/DataFixtures/ORM/Scenarios/Scenario_2/ --append
+bin/console doctrine:fixtures:load --fixtures=src/AppBundle/DataFixtures/ORM/Scenarios/Scenario_3/ --append
+bin/console doctrine:fixtures:load --fixtures=src/AppBundle/DataFixtures/ORM/Scenarios/Scenario_4/ --append
--- build/loadFixtures.osx.sh
+++ build/loadFixtures.osx.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-bin/console doctrine:fixtures:load -n --fixtures=src/AppBundle/DataFixtures/ORM/Scenarios/PropertyTypes
+./build.osx.sh
+
+bin/console doctrine:fixtures:load --fixtures=src/AppBundle/DataFixtures/ORM/Scenarios/PropertyTypes --append
 bin/console doctrine:fixtures:load --fixtures=src/AppBundle/DataFixtures/ORM/TestScenarios/Scenario_1_Lake_Example --append
 bin/console doctrine:fixtures:load --fixtures=src/AppBundle/DataFixtures/ORM/Scenarios/Scenario_1/ --append
 bin/console doctrine:fixtures:load --fixtures=src/AppBundle/DataFixtures/ORM/Scenarios/Scenario_2/ --append
