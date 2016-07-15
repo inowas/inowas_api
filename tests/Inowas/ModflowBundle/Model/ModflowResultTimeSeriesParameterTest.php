fdcd94ac06f89adcdf51a3851971c1f1c0b115ff
Ralf Junghanns <ralf.junghanns@gmail.com>
2016-07-15 22:55:25 +0200

    Move some more classes to ModflowBundle and add tests

---
+++ tests/Inowas/ModflowBundle/Model/ModflowResultTimeSeriesParameterTest.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ralf
+ * Date: 15.07.16
+ * Time: 22:51
+ */
+
+namespace Tests\Inowas\ModflowBundle\Model;
+
+
+class ModflowResultTimeSeriesParameterTest extends \PHPUnit_Framework_TestCase
+{
+
+}
