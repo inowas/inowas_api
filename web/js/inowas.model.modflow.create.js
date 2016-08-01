afc69c418404a7eed0aca6d80674739c3a214d15
Ralf Junghanns <ralf.junghanns@gmail.com>
2016-08-01 11:51:30 +0200

    Add area-creator

--- web/js/inowas.model.modflow.create.js
+++ web/js/inowas.model.modflow.create.js
@@ -1,49 +1,63 @@
 // initialize the map on the "map" div with a given center and zoom
-var area_map = L.map('area-map').setView(
-    [-31.36989525516968, -63.6753815432894], 11
-);
+//var area_map = L.map('area-map').setView([-31.36989525516968, -63.6753815432894], 11);
 
+// Instantiate Map
+var area_map = new L.Map('area-map', {zoom: 9, center: new L.latLng([41.575730,13.002411]) });
 
-var streets = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpandmbXliNDBjZWd2M2x6bDk3c2ZtOTkifQ._QA7i5Mpkd_m30IGElHziw', {
+// Add layers
+area_map.addLayer(L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpandmbXliNDBjZWd2M2x6bDk3c2ZtOTkifQ._QA7i5Mpkd_m30IGElHziw', {
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
     '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
     'Imagery © <a href="http://mapbox.com">Mapbox</a>',
     id: 'mapbox.streets'
-}).addTo(area_map);
-var Hydda_Full = L.tileLayer('http://{s}.tile.openstreetmap.se/hydda/full/{z}/{x}/{y}.png', {
-    attribution: 'Tiles courtesy of <a href="http://openstreetmap.se/" target="_blank">OpenStreetMap Sweden</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-});
+}));
+
+// Add search control
+area_map.addControl( new L.Control.Search({
+    url: 'http://nominatim.openstreetmap.org/search?format=json&q={s}',
+    jsonpParam: 'json_callback',
+    propertyName: 'display_name',
+    propertyLoc: ['lat','lon'],
+    markerLocation: true,
+    autoCollapse: true,
+    autoType: false,
+    minLength: 2,
+    position: 'topright'
+}) );
 
 // Initialise the FeatureGroup to store editable layers
 var drawnItems = new L.FeatureGroup();
 area_map.addLayer(drawnItems);
 
-// Initialise the draw control and pass it the FeatureGroup of editable layers
-var drawControl = new L.Control.Draw({
+var drawControlFull = new L.Control.Draw({
     draw : {
-        position : 'topleft'
-    },
+        position: 'topleft',
+        polyline: false,
+        marker: false
+    }
+});
+
+var drawControlEditOnly = new L.Control.Draw({
     edit: {
         featureGroup: drawnItems
-    }
+    },
+    draw: false
 });
 
-area_map.addControl(drawControl);
+area_map.addControl(drawControlFull);
 
-area_map.on('draw:created', function (e) {
-    var type = e.layerType,
-        layer = e.layer;
-
-    console.log(e);
-
-    if (type === 'rectangle') {
-        console.log(e);
-        console.log(e.layer._latlngs[0].distanceTo(e.layer._latlngs[1]));
-        console.log(e.layer._latlngs[0].distanceTo(e.layer._latlngs[3]));
-        // Do marker specific actions
-    }
-
-    // Do whatever else you need to. (save to db, add to map etc)
-    area_map.addLayer(layer);
+area_map.on("draw:created", function (e) {
+    var layer = e.layer;
+    layer.addTo(drawnItems);
+    drawControlFull.removeFrom(area_map);
+    drawControlEditOnly.addTo(area_map)
 });
+
+area_map.on("draw:deleted", function(e) {
+    drawControlEditOnly.removeFrom(area_map);
+    drawControlFull.addTo(area_map);
+});
+
+
+
