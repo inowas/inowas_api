6bbe4abbbd05d56e25c86403ce42d1ebf9120c43
Ralf Junghanns <ralf.junghanns@gmail.com>
2016-08-03 14:15:41 +0200

    Update CreateModflowModelRestControllerTest

--- tests/AppBundle/Controller/CreateModflowModelRestControllerTest.php
+++ tests/AppBundle/Controller/CreateModflowModelRestControllerTest.php
@@ -27,7 +27,7 @@ class CreateModflowModelRestControllerTest extends WebTestCase
         $client->request(
             'POST',
             '/api/modflowmodels.json',
-            array('json' => '{"area":{"geoJSON":"{\"type\":\"Feature\",\"properties\":{},\"geometry\":{\"type\":\"Polygon\",\"coordinates\":[[[12.564239501953123,41.72213058512578],[13.119049072265625,41.90840946591109],[13.594207763671875,41.51269075845857],[13.168487548828123,41.475660200278234],[13.105316162109375,41.759019938155404],[12.808685302734375,41.60722821271717],[12.564239501953123,41.72213058512578]]]}}"},"grid_size":{"cols":"100","rows":"100"},"soil_model":{"numberOfLayers":"3"},"name":"myModelName","description":"myModelDescription"}')
+            array('json' => '{"area":{"geoJSON":"{\"type\":\"Feature\",\"properties\":{},\"geometry\":{\"type\":\"Polygon\",\"coordinates\":[[[12.564239501953123,41.72213058512578],[13.119049072265625,41.90840946591109],[13.594207763671875,41.51269075845857],[13.168487548828123,41.475660200278234],[13.105316162109375,41.759019938155404],[12.808685302734375,41.60722821271717],[12.564239501953123,41.72213058512578]]]}}"},"grid_size":{"cols":"10","rows":"10"},"soil_model":{"numberOfLayers":"1"},"name":"myModelName","description":"myModelDescription"}')
         );
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
         $response = $client->getResponse()->getContent();
