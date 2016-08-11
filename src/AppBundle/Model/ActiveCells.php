42c2e05e95ab893ee5a14436a843f515b10a88d3
Ralf Junghanns <ralf.junghanns@gmail.com>
2016-08-11 14:53:51 +0200

    Improve ActiveCells-Object

--- src/AppBundle/Model/ActiveCells.php
+++ src/AppBundle/Model/ActiveCells.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Model;
 
+use AppBundle\Exception\InvalidArgumentException;
 use JMS\Serializer\Annotation as JMS;
 
 /**
@@ -23,16 +24,58 @@ class ActiveCells
     public static function fromArray(array $cells)
     {
         $instance = new self();
-        
-        if (!is_array($cells[0])){
+
+        if (!is_array($cells)){
             throw new \InvalidArgumentException(sprintf(
                 'ActiveCells is supposed to be an two dimensional array, %s given',
                 gettype($cells)
             ));
         }
 
+        foreach ($cells as $cell){
+            if (!is_array($cell)){
+                throw new \InvalidArgumentException(sprintf(
+                    'ActiveCells is supposed to be an two dimensional array, %s given',
+                    gettype($cell)
+                ));
+            }
+        }
+
         $instance->cells = $cells;
         return $instance;
+    }
+
+    public static function fromObject($obj)
+    {
+        $cells = array();
+        foreach ($obj as $row => $cols){
+            foreach ($cols as $col => $value){
+                $cells[intval($row)][intval($col)] = $value;
+            }
+        }
+
+        return self::fromArray($cells);
+    }
+
+    public static function fromJSON($json){
+        $decodedJson = json_decode($json);
+
+        if ($decodedJson == null){
+            throw new InvalidArgumentException(sprintf(
+                'Argument is supposed to be a valid JSON-String, %s given.',
+                $json
+            ));
+        }
+
+        if (is_array($decodedJson)){
+            return self::fromArray($decodedJson);
+        }
+
+        if (is_object($decodedJson)){
+            return self::fromObject($decodedJson);
+        }
+
+        return null;
     }
 
     public function toArray()
--- tests/AppBundle/Model/ActiveCellsTest.php
+++ tests/AppBundle/Model/ActiveCellsTest.php
@@ -15,10 +15,44 @@ class ActiveCellsTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals($data,$activeCells->toArray());
     }
 
-    public function testThrowExceptionIfNotAnTwoDimansionalArray(){
+    public function testCreateFromArrayWithSingleFieldsSet(){
+        $data[1][1] = true;
+        $activeCells = ActiveCells::fromArray($data);
+        $this->assertInstanceOf('AppBundle\Model\ActiveCells', $activeCells);
+        $this->assertEquals($data,$activeCells->toArray());
+    }
+
+    public function testCreateFromObject(){
+        $data = array("2" => array(2=>1, 3=>2, 5=>3));
+        $activeCells = ActiveCells::fromObject((object)$data);
+        $this->assertInstanceOf('AppBundle\Model\ActiveCells', $activeCells);
+        $this->assertEquals($data,$activeCells->toArray());
+    }
+
+    public function testCreateFromJsonArray(){
+        $data = array("2" => array(2=>1, 3=>2, 5=>3));
+        $activeCells = ActiveCells::fromJSON(json_encode($data));
+        $this->assertInstanceOf('AppBundle\Model\ActiveCells', $activeCells);
+        $this->assertEquals($data,$activeCells->toArray());
+    }
+
+    public function testCreateFromJsonObject(){
+        $data = array("2" => array(2=>1, 3=>2, 5=>3));
+        $activeCells = ActiveCells::fromJSON(json_encode((object)$data));
+        $this->assertInstanceOf('AppBundle\Model\ActiveCells', $activeCells);
+        $this->assertEquals($data,$activeCells->toArray());
+    }
+
+    public function testThrowExceptionIfNotAnTwoDimensionalArray(){
         $data = array(1,2,3);
         $this->setExpectedException('InvalidArgumentException');
         ActiveCells::fromArray($data);
+    }
+
+    public function testThrowExceptionWithInvalidJson(){
+        $data = "{{ 6: 239iu4";
+        $this->setExpectedException('InvalidArgumentException');
+        ActiveCells::fromJSON($data);
     }
 
 }
