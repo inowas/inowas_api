9ba8fb79e6e628da6988d0ccdfc8aecd574cee72
Ralf Junghanns <ralf.junghanns@gmail.com>
2016-08-22 16:57:25 +0200

    OcPackageAdapter

--- src/Inowas/PyprocessingBundle/Model/Modflow/Package/OcPackageFactory.php
+++ src/Inowas/PyprocessingBundle/Model/Modflow/Package/OcPackageFactory.php
@@ -7,6 +7,20 @@ use AppBundle\Entity\ModFlowModel;
 class OcPackageFactory implements PackageFactoryInterface
 {
     public function create(ModFlowModel $model){
-        return new OcPackage();
+
+        $oc = new OcPackage();
+        $adapter = new OcPackageAdapter($model);
+
+        $oc->setIhedfm($adapter->getIhedfm());
+        $oc->setIddnfm($adapter->getIddnfm());
+        $oc->setChedfm($adapter->getChedfm());
+        $oc->setCddnfm($adapter->getCddnfm());
+        $oc->setCboufm($adapter->getCboufm());
+        $oc->setCompact($adapter->isCompact());
+        $oc->setStressPeriodData($adapter->getStressPeriodData());
+        $oc->setExtension($adapter->getExtension());
+        $oc->setUnitnumber($adapter->getUnitnumber());
+
+        return $oc;
     }
 }
