a0bc5860d4fe2a25c52f3b4f3381db05e0f17d32
Ralf Junghanns <ralf.junghanns@gmail.com>
2016-08-22 15:04:29 +0200

    Add PcgPackageAdapter

--- src/Inowas/PyprocessingBundle/Model/Modflow/Package/PcgPackageFactory.php
+++ src/Inowas/PyprocessingBundle/Model/Modflow/Package/PcgPackageFactory.php
@@ -7,6 +7,25 @@ use AppBundle\Entity\ModFlowModel;
 class PcgPackageFactory implements PackageFactoryInterface
 {
     public function create(ModFlowModel $model){
-        return new PcgPackage();
+
+        $pcg = new PcgPackage();
+        $adapter = new PcgPackageAdapter($model);
+
+        $pcg->setMxiter($adapter->getMxiter());
+        $pcg->setIter1($adapter->getIter1());
+        $pcg->setNpcond($adapter->getNpcond());
+        $pcg->setHclose($adapter->getHclose());
+        $pcg->setRclose($adapter->getRclose());
+        $pcg->setRelax($adapter->getRelax());
+        $pcg->setNbpol($adapter->getNbpol());
+        $pcg->setIprpcg($adapter->getIprpcg());
+        $pcg->setMutpcg($adapter->getMutpcg());
+        $pcg->setDamp($adapter->getDamp());
+        $pcg->setDampt($adapter->getDampt());
+        $pcg->setIhcofadd($adapter->getIhcofadd());
+        $pcg->setExtension($adapter->getExtension());
+        $pcg->setUnitnumber($adapter->getUnitnumber());
+
+        return $pcg;
     }
 }
