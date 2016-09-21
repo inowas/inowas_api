59f26d937c58f7d2cfcf27cf82a2bf110e797946
Ralf Junghanns <ralf.junghanns@gmail.com>
2016-09-21 16:35:26 +0200

    Remove some duplicated code

--- src/Inowas/PyprocessingBundle/Command/FlopyCalculateCommand.php
+++ src/Inowas/PyprocessingBundle/Command/FlopyCalculateCommand.php
@@ -4,14 +4,12 @@ namespace Inowas\PyprocessingBundle\Command;
 
 use AppBundle\Entity\ModFlowModel;
 use Inowas\PyprocessingBundle\Model\Modflow\Package\FlopyCalculationPropertiesFactory;
-use Ramsey\Uuid\Uuid;
-use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class FlopyCalculateCommand extends ContainerAwareCommand
+class FlopyCalculateCommand extends FlopyCommand
 {
 
     protected function configure()
@@ -62,6 +60,8 @@ class FlopyCalculateCommand extends ContainerAwareCommand
         $response = $this->getModelFromInput($input, $output);
 
         if ($response instanceof ModFlowModel){
+
+            /** @var ModFlowModel $model */
             $model = $response;
             $output->writeln(sprintf("Calculating model id: %s", $input->getArgument('id')));
 
@@ -109,52 +109,5 @@ class FlopyCalculateCommand extends ContainerAwareCommand
             $output->writeln($process->getErrorOutput());
         }
         return 1;
-    }
-
-    private function getModelFromInput(InputInterface $input, OutputInterface $output){
-        if (! $input->getArgument('id')){
-            $modflowModels = $this->getContainer()->get('doctrine.orm.default_entity_manager')->getRepository('AppBundle:ModFlowModel')
-                ->findBy(
-                    array(),
-                    array('dateCreated' => 'ASC')
-                );
-
-            $counter = 0;
-            /** @var ModFlowModel $modflowModel */
-            foreach ($modflowModels as $modflowModel) {
-                $output->writeln(sprintf("#%s, ID: %s, Name: %s, Owner: %s ", ++$counter, $modflowModel->getId()->toString(), $modflowModel->getName(), $modflowModel->getOwner()));
-            }
-
-            return 1;
-        }
-
-        if (Uuid::isValid($input->getArgument('id'))){
-            $model = $this->getContainer()->get('doctrine.orm.default_entity_manager')
-                ->getRepository('AppBundle:ModFlowModel')
-                ->findOneBy(array(
-                    'id' => $input->getArgument('id')
-                ));
-
-            if (! $model instanceof ModFlowModel){
-                $output->writeln(sprintf("The given id: %s is not a valid Model", $input->getArgument('id')));
-                return 0;
-            }
-
-        } else {
-            $modflowModels = $this->getContainer()->get('doctrine.orm.default_entity_manager')->getRepository('AppBundle:ModFlowModel')
-                ->findBy(
-                    array(),
-                    array('dateCreated' => 'ASC')
-                );
-
-            if (count($modflowModels) < $input->getArgument('id')){
-                $output->writeln(sprintf("The given id: %s is not valid", $input->getArgument('id')));
-                return 0;
-            }
-
-            $model = $modflowModels[$input->getArgument('id')-1];
-        }
-
-        return $model;
     }
 }
--- src/Inowas/PyprocessingBundle/Command/FlopyValidateCommand.php
+++ src/Inowas/PyprocessingBundle/Command/FlopyValidateCommand.php
@@ -4,13 +4,11 @@ namespace Inowas\PyprocessingBundle\Command;
 
 use AppBundle\Entity\ModFlowModel;
 use Inowas\PyprocessingBundle\Model\Modflow\Package\PackageFactory;
-use Ramsey\Uuid\Uuid;
-use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class FlopyValidateCommand extends ContainerAwareCommand
+class FlopyValidateCommand extends FlopyCommand
 {
 
     protected function configure()
@@ -34,52 +32,15 @@ class FlopyValidateCommand extends ContainerAwareCommand
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        if (! $input->getArgument('id')){
-            $modflowModels = $this->getContainer()->get('doctrine.orm.default_entity_manager')->getRepository('AppBundle:ModFlowModel')
-                ->findBy(
-                    array(),
-                    array('dateCreated' => 'ASC')
-                );
 
-            $counter = 0;
-            /** @var ModFlowModel $modflowModel */
-            foreach ($modflowModels as $modflowModel) {
-                $output->writeln(sprintf("#%s, ID: %s, Name: %s, Owner: %s ", ++$counter, $modflowModel->getId()->toString(), $modflowModel->getName(), $modflowModel->getOwner()));
-            }
+        $response = $this->getModelFromInput($input, $output);
 
-            return 1;
+        if ($response instanceof ModFlowModel) {
+            $model = $response;
+            $package = PackageFactory::create($input->getArgument('package'), $model);
+            $output->writeln(json_encode($package));
         }
 
-        if (Uuid::isValid($input->getArgument('id'))){
-            $model = $this->getContainer()->get('doctrine.orm.default_entity_manager')
-                ->getRepository('AppBundle:ModFlowModel')
-                ->findOneBy(array(
-                    'id' => $input->getArgument('id')
-                ));
-
-            if (! $model instanceof ModFlowModel){
-                $output->writeln(sprintf("The given id: %s is not a valid Model", $input->getArgument('id')));
-                return 0;
-            }
-
-
-        } else {
-            $modflowModels = $this->getContainer()->get('doctrine.orm.default_entity_manager')->getRepository('AppBundle:ModFlowModel')
-                ->findBy(
-                    array(),
-                    array('dateCreated' => 'ASC')
-                );
-
-            if (count($modflowModels) < $input->getArgument('id')){
-                $output->writeln(sprintf("The given id: %s is not valid", $input->getArgument('id')));
-                return 0;
-            }
-
-            $model = $modflowModels[$input->getArgument('id')-1];
-        }
-
-        $package = PackageFactory::create($input->getArgument('package'), $model);
-        $output->writeln(json_encode($package));
         return 1;
     }
 }
