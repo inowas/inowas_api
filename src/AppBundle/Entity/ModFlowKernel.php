ade2a19de3cc279788f45893c58750c3f8b27924
Ralf Junghanns <ralf.junghanns@gmail.com>
2016-09-22 15:17:56 +0200

    Update ModlowKernel

--- src/AppBundle/Entity/ModFlowKernel.php
+++ src/AppBundle/Entity/ModFlowKernel.php
@@ -2,13 +2,44 @@
 
 namespace AppBundle\Entity;
 
+use Doctrine\ORM\Mapping as ORM;
+use JMS\Serializer\Annotation as JMS;
+
 /**
  * ModFlowKernel
+ *
+ * @ORM\Entity()
+ * @JMS\ExclusionPolicy("all")
  */
 class ModFlowKernel extends AbstractKernel
 {
     public function __construct()
     {
         parent::__construct();
+    }
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255, nullable=true)
+     */
+    private $name;
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     * @return ModFlowKernel
+     */
+    public function setName(string $name): ModFlowKernel
+    {
+        $this->name = $name;
+        return $this;
     }
 }
