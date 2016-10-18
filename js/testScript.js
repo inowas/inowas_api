a850ab1fa935d9e7f6108e4d102d3034853528ab
Ralf Junghanns <ralf.junghanns@gmail.com>
2016-10-18 13:35:53 +0200

    Add phantomJS-Script to parse html

---
+++ js/testScript.js
@@ -0,0 +1,1 @@
+var system = require('system');var args = system.args;if (args.length === 1) {    console.log('Try to pass some arguments when invoking this script!');} else {    args.forEach(function(arg, i) {        console.log(i + ': ' + arg);    });}
