2d50660837816f5de31c780c8d65001d77b74bd5
Ralf Junghanns <ralf.junghanns@gmail.com>
2016-12-15 08:39:53 +0100

    Add factory and scenariotest

--- src/Inowas/ScenarioAnalysisBundle/Factory/ScenarioFactory.php
+++ src/Inowas/ScenarioAnalysisBundle/Factory/ScenarioFactory.php
@@ -1,15 +1,15 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Ralf
- * Date: 15.12.16
- * Time: 08:29
- */
 
 namespace Inowas\ScenarioAnalysisBundle\Factory;
 
+use Inowas\ModflowBundle\Model\ModflowModel;
+use Inowas\ScenarioAnalysisBundle\Model\Scenario;
 
 class ScenarioFactory
 {
+    final private function __construct(){}
 
+    public static function create(ModflowModel $model){
+        return new Scenario($model);
+    }
 }
--- src/Inowas/ScenarioAnalysisBundle/Tests/Model/ScenarioTest.php
+++ src/Inowas/ScenarioAnalysisBundle/Tests/Model/ScenarioTest.php
@@ -1,18 +1,36 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Ralf
- * Date: 15.12.16
- * Time: 08:28
- */
 
 namespace Inowas\ScenarioAnalysisBundle\Tests\Model;
 
 
+use Inowas\ModflowBundle\Model\ModflowModel;
+use Inowas\ModflowBundle\Model\ModflowModelFactory;
+use Inowas\ScenarioAnalysisBundle\Factory\ScenarioFactory;
 use Inowas\ScenarioAnalysisBundle\Model\Scenario;
-
 
 class ScenarioTest extends \PHPUnit_Framework_TestCase
 {
+    /** @var  ModflowModel */
+    protected $model;
 
+    /** @var  Scenario */
+    protected $scenario;
+
+    public function setUp()
+    {
+        $this->model = ModflowModelFactory::create();
+        $this->scenario =  ScenarioFactory::create($this->model);
+    }
+
+    public function testCanInstantiateScenario()
+    {
+        $this->assertInstanceOf(ModflowModel::class, $this->model);
+        $this->assertInstanceOf(Scenario::class, $this->scenario);
+    }
+
+    public function tearDown()
+    {
+        unset($this->model);
+        unset($this->scenario);
+    }
 }
