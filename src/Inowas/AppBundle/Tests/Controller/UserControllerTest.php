fe364eb77fe7249721c315dddbd97f7dde7ec19e
Ralf Junghanns <ralf.junghanns@gmail.com>
2016-12-16 17:42:31 +0100

    Add route and configuration to retrieve usercredentials

---
+++ src/Inowas/AppBundle/Tests/Controller/UserControllerTest.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Inowas\AppBundle\Tests\Controller;
+
+use FOS\UserBundle\Doctrine\UserManager;
+use Inowas\AppBundle\Model\User;
+use Ramsey\Uuid\Uuid;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class UserControllerTest extends WebTestCase
+{
+
+    /** @var  User */
+    protected $user;
+
+    /** @var  UserManager */
+    protected $userManager;
+
+    public function setUp(){
+
+        self::bootKernel();
+
+        $this->userManager = static::$kernel->getContainer()
+            ->get('fos_user.user_manager')
+        ;
+
+        $username = 'testUser';
+        $email = 'testUser@email';
+        $password = 'testUserPassword';
+
+        $this->user = $this->userManager->findUserByUsername($username);
+        if (! $this->user instanceof User){
+            $this->user = $this->userManager->createUser()
+                ->setUsername($username)
+                ->setEmail($email)
+                ->setPlainPassword($password)
+                ->setEnabled(true);
+
+            $this->userManager->updateUser($this->user);
+        }
+    }
+
+    public function testRetrieveAPIKeyWithCorrectCredentials(){
+
+        $client = static::createClient();
+        $client->request(
+            'POST',
+            '/api/users/credentials.json',
+            array(
+                'username' => $this->user->getUsername(),
+                'password' => 'testUserPassword'
+            )
+        );
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $response = $client->getResponse()->getContent();
+        $this->assertJson($response);
+        $response = json_decode($response);
+        $this->assertEquals($this->user->getApiKey(), $response->api_key);
+    }
+
+    public function testRetrieveAPIKeyWithInCorrectCredentialsReturns401(){
+
+        $client = static::createClient();
+        $client->request(
+            'POST',
+            '/api/users/credentials.json',
+            array(
+                'username' => $this->user->getUsername(),
+                'password' => 'sadkjfh'
+            )
+        );
+
+        $this->assertEquals(401, $client->getResponse()->getStatusCode());
+    }
+}
