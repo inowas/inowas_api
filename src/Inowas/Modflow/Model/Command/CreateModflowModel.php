df787a7e3004f7ab127d395e39c78560b59def13
Ralf Junghanns <ralf.junghanns@gmail.com>
2017-02-10 11:42:49 +0100

    Add createModflowModel command

---
+++ src/Inowas/Modflow/Model/Command/CreateModflowModel.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Inowas\Modflow\Model\Command;
+
+use Inowas\Modflow\Model\ModflowModelId;
+use Prooph\Common\Messaging\Command;
+use Prooph\Common\Messaging\PayloadConstructable;
+use Prooph\Common\Messaging\PayloadTrait;
+
+class CreateModflowModel extends Command implements PayloadConstructable
+{
+
+    use PayloadTrait;
+
+    public static function withId(ModflowModelId $modelId): CreateModflowModel
+    {
+        return new self(
+            [
+                'modflow_model_id' => $modelId->toString()
+            ]
+        );
+    }
+
+    public function modflowModelId(): ModflowModelId
+    {
+        return ModflowModelId::fromString($this->payload['modflow_model_id']);
+    }
+}
