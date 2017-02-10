b13fe1af2bc6cceed4320c289b35ecfe884fcbb7
Ralf Junghanns <ralf.junghanns@gmail.com>
2017-02-10 12:05:49 +0100

    Add command changeModflowModelDescription

---
+++ src/Inowas/Modflow/Model/Command/ChangeModflowModelDescription.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Inowas\Modflow\Model\Command;
+
+use Inowas\Modflow\Model\ModflowModelDescription;
+use Inowas\Modflow\Model\ModflowModelId;
+use Prooph\Common\Messaging\Command;
+use Prooph\Common\Messaging\PayloadConstructable;
+use Prooph\Common\Messaging\PayloadTrait;
+
+class ChangeModflowModelDescription extends Command implements PayloadConstructable
+{
+
+    use PayloadTrait;
+
+    public static function forModflowModel(ModflowModelId $modelId, ModflowModelDescription $description): ChangeModflowModelDescription
+    {
+        return new self(
+            [
+                'modflow_model_id' => $modelId->toString(),
+                'description' => $description->toString()
+            ]
+        );
+    }
+
+    public function modflowModelId(): ModflowModelId
+    {
+        return ModflowModelId::fromString($this->payload['modflow_model_id']);
+    }
+
+    public function name(): ModflowModelDescription
+    {
+        return ModflowModelDescription::fromString($this->payload['description']);
+    }
+}
