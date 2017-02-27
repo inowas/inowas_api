4d4a7b6b16af2efc5829cbf8d27eb6f14c9a58e6
Ralf Junghanns <ralf.junghanns@gmail.com>
2017-02-27 09:44:11 +0100

    Add modelDetailsFinder for ModelDetailsProjection

---
+++ src/Inowas/Modflow/Projection/ModelScenarioList/ModelDetailsFinder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Inowas\Modflow\Projection\ModelScenarioList;
+
+use Doctrine\DBAL\Connection;
+use Inowas\Modflow\Model\ModflowId;
+use Inowas\Modflow\Model\UserId;
+use Inowas\Modflow\Projection\Table;
+
+class ModelDetailsFinder
+{
+    /** @var Connection $connection */
+    protected $connection;
+
+    public function __construct(Connection $connection) {
+        $this->connection = $connection;
+        $this->connection->setFetchMode(\PDO::FETCH_OBJ);
+    }
+
+    public function findByBaseModelId(ModflowId $modelId)
+    {
+        return $this->connection->fetchAll(
+            sprintf('SELECT * FROM %s WHERE model_id = :model_id', Table::MODEL_DETAILS),
+            ['model_id' => $modelId->toString()]
+        );
+    }
+}
