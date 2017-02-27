817c630b9aec71abe8f5e8be60d9160a7da350bd
Ralf Junghanns <ralf.junghanns@gmail.com>
2017-02-27 08:56:41 +0100

    Update ModelScenarioListProjection

--- src/Inowas/Modflow/Projection/ModelScenarioList/ModelScenarioListProjector.php
+++ src/Inowas/Modflow/Projection/ModelScenarioList/ModelScenarioListProjector.php
@@ -5,8 +5,6 @@ namespace Inowas\Modflow\Projection\ModelScenarioList;
 use Doctrine\DBAL\Connection;
 use Doctrine\DBAL\Exception\TableNotFoundException;
 use Doctrine\DBAL\Schema\Schema;
-use Inowas\Modflow\Model\Event\BoundaryWasAdded;
-use Inowas\Modflow\Model\Event\ModflowCalculationWasCreated;
 use Inowas\Modflow\Model\Event\ModflowModelDescriptionWasChanged;
 use Inowas\Modflow\Model\Event\ModflowModelNameWasChanged;
 use Inowas\Modflow\Model\Event\ModflowModelWasCreated;
@@ -35,13 +33,10 @@ class ModelScenarioListProjector implements ProjectionInterface
         $table->addColumn('user_id', 'string', ['length' => 36]);
         $table->addColumn('base_model_id', 'string', ['length' => 36]);
         $table->addColumn('scenario_id', 'string', ['length' => 36]);
-        $table->addColumn('calculation_id', 'string', ['length' => 36, 'notnull' => false]);
         $table->addColumn('name', 'string', ['length' => 255]);
         $table->addColumn('description', 'string', ['length' => 255]);
-        $table->addColumn('area_geometry', 'text', ['notnull' => false]);
-        $table->addColumn('grid_size', 'text', ['notnull' => false]);
-        $table->addColumn('bounding_box', 'text', ['notnull' => false]);
         $table->setPrimaryKey(['id']);
+        $table->addIndex(array('base_model_id'));
     }
 
     public function createTable(): void
@@ -108,23 +103,12 @@ class ModelScenarioListProjector implements ProjectionInterface
 
     public function onModflowScenarioWasAdded(ModflowScenarioWasAdded $event): void
     {
-        $sql = sprintf("SELECT area_geometry, grid_size FROM %s WHERE base_model_id = ? AND user_id = ?", Table::MODEL_SCENARIO_LIST);
-        $stmt = $this->connection->prepare($sql);
-        $stmt->bindValue(1, $event->baseModelId()->toString());
-        $stmt->bindValue(2, $event->userId()->toString());
-        $stmt->execute();
-        $result = $stmt->fetchAll()[0];
-        $area_geometry = $result['area_geometry'];
-        $grid_size = $result['grid_size'];
-
         $this->connection->insert(Table::MODEL_SCENARIO_LIST, array(
             'user_id' => $event->userId()->toString(),
             'base_model_id' => $event->baseModelId()->toString(),
             'scenario_id' => $event->scenarioId()->toString(),
             'name' => '',
-            'description' => '',
-            'area_geometry' => $area_geometry,
-            'grid_size' => $grid_size
+            'description' => ''
         ));
     }
 
@@ -148,30 +132,5 @@ class ModelScenarioListProjector implements ProjectionInterface
                 'scenario_id' => $event->scenarioId()->toString()
             )
         );
-    }
-
-    public function onModflowCalculationWasCreated(ModflowCalculationWasCreated $event){
-        $this->connection->update(Table::MODEL_SCENARIO_LIST,
-            array('calculation_id' => $event->calculationId()->toString()),
-            array(
-                'base_model_id' => $event->modflowModelId()->toString(),
-                'scenario_id' => ''
-            )
-        );
-
-        $this->connection->update(Table::MODEL_SCENARIO_LIST,
-            array('calculation_id' => $event->calculationId()->toString()),
-            array('scenario_id' => $event->modflowModelId()->toString())
-        );
-    }
-
-    public function onBoundaryWasAdded(BoundaryWasAdded $event){
-        $boundary = $event->boundary();
-        if ($boundary->type() == 'area') {
-            $this->connection->update(Table::MODEL_SCENARIO_LIST,
-                array('area_geometry' => $boundary->geometry()->toJson()),
-                array('base_model_id' => $event->modflowId()->toString())
-            );
-        }
     }
 }
