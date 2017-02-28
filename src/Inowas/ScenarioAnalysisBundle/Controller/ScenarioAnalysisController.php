1eac34775e95c6d7c288d95363d70edafc375b24
Ralf Junghanns <ralf.junghanns@gmail.com>
2017-02-28 09:47:49 +0100

    Add routes for image rendering

--- src/Inowas/ScenarioAnalysisBundle/Controller/ScenarioAnalysisController.php
+++ src/Inowas/ScenarioAnalysisBundle/Controller/ScenarioAnalysisController.php
@@ -4,6 +4,8 @@ namespace Inowas\ScenarioAnalysisBundle\Controller;
 
 use FOS\RestBundle\Controller\Annotations as Rest;
 use FOS\RestBundle\Controller\FOSRestController;
+use FOS\RestBundle\Request\ParamFetcher;
+use HeatMap\HeatMap;
 use Inowas\Modflow\Model\CalculationResultType;
 use Inowas\Modflow\Model\LayerNumber;
 use Inowas\Modflow\Model\ModflowId;
@@ -14,6 +16,7 @@ use Inowas\ScenarioAnalysisBundle\Exception\InvalidUuidException;
 use Nelmio\ApiDocBundle\Annotation\ApiDoc as ApiDoc;
 use Ramsey\Uuid\Uuid;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 
 class ScenarioAnalysisController extends FOSRestController
 {
@@ -246,14 +249,13 @@ class ScenarioAnalysisController extends FOSRestController
      *   }
      * )
      *
-     * @Rest\Get("/model/calculation/{calculationId}/result/type/{type}/layer/{layer}/totim/{totim}")
+     * @Rest\Route("/model/calculation/{calculationId}/result/type/{type}/layer/{layer}/totim/{totim}", methods={"GET"}, requirements={"_format"="json"})
      * @param $calculationId
      * @param $type
      * @param $layer
      * @param $totim
      * @return JsonResponse
      * @throws InvalidUuidException
-     * @throws InvalidArgumentException
      */
     public function getScenarioAnalysisResultByCalculationResultTypeAndLayerAction($calculationId, $type, $layer, $totim)
     {
@@ -333,5 +335,118 @@ class ScenarioAnalysisController extends FOSRestController
         return new JsonResponse($layerValues);
     }
 
+    /**
+     * Get calculation result as image by calculationId, type and layerNumber.
+     *
+     * @ApiDoc(
+     *   resource = true,
+     *   description = "Get calculation result as image by calculationId, type and layerNumber.",
+     *   statusCodes = {
+     *     200 = "Returned when successful",
+     *     404 = "Returned when the Calculation-Id is not found"
+     *   }
+     * )
+     *
+     * @Rest\Route("/model/{modelId}/calculation/result/type/{type}/layer/{layer}/totim/{totim}", methods={"GET"}, requirements={"_format"="png"})
+     *
+     * @param ParamFetcher $paramFetcher
+     * @param $modelId
+     * @param $type
+     * @param $layer
+     * @param $totim
+     * @return Response
+     * @throws InvalidUuidException
+     *
+     * @Rest\QueryParam(name="max", default=null, description="Value of the spectrum maximum")
+     * @Rest\QueryParam(name="min", default=null, description="Value of the spectrum minimum")
+     * @Rest\QueryParam(name="upper", requirements="\d+", default=95, description="Percentile spectrum max")
+     * @Rest\QueryParam(name="loper", requirements="\d+", default=5, description="Percentile spectrum min")
+     */
+    public function getScenarioAnalysisResultImageByModelIdIdTypeLayerAndTotimAction(ParamFetcher $paramFetcher, $modelId, $type, $layer, $totim): Response
+    {
 
+        if (! Uuid::isValid($modelId)){
+            throw new InvalidUuidException();
+        }
+
+        $calculation = $this->get('inowas.modflow_projection.calculation_list_finder')
+            ->findLastCalculationByModelId(ModflowId::fromString($modelId));
+
+        $result = $this->get('inowas.modflow_projection.calculation_results_finder')
+            ->findValue(
+                ModflowId::fromString($calculation['calculation_id']),
+                CalculationResultType::fromString($type),
+                LayerNumber::fromInteger((int)$layer),
+                TotalTime::fromInt((int)$totim)
+            );
+
+
+        $heatMap = new HeatMap();
+        if ($paramFetcher->get('min') && $paramFetcher->get('max')){
+            $file = $heatMap->createWithAbsoluteLimits($result->data()->toArray(), $paramFetcher->get('min'), $paramFetcher->get('max'));
+        } else {
+            $file = $heatMap->createWithPercentileLimits($result->data()->toArray(), $paramFetcher->get('loper'), $paramFetcher->get('upper'));
+        }
+
+        $response = new Response();
+        $response->headers->set('Content-Type', 'image/png');
+        $response->setContent(file_get_contents($file));
+        return $response;
+    }
+
+    /**
+     * Get calculation result as image by calculationId, type and layerNumber.
+     *
+     * @ApiDoc(
+     *   resource = true,
+     *   description = "Get calculation result as image by calculationId, type and layerNumber.",
+     *   statusCodes = {
+     *     200 = "Returned when successful",
+     *     404 = "Returned when the Calculation-Id is not found"
+     *   }
+     * )
+     *
+     * @Rest\Route("/model/calculation/{calculationId}/result/type/{type}/layer/{layer}/totim/{totim}", methods={"GET"}, requirements={"_format"="png"})
+     *
+     * @param ParamFetcher $paramFetcher
+     * @param $calculationId
+     * @param $type
+     * @param $layer
+     * @param $totim
+     * @return Response
+     * @throws InvalidUuidException
+     *
+     * @Rest\QueryParam(name="max", default=null, description="Value of the spectrum maximum")
+     * @Rest\QueryParam(name="min", default=null, description="Value of the spectrum minimum")
+     * @Rest\QueryParam(name="upper", requirements="\d+", default=95, description="Percentile spectrum max")
+     * @Rest\QueryParam(name="loper", requirements="\d+", default=5, description="Percentile spectrum min")
+     */
+    public function getScenarioAnalysisResultImageByCalculationIdTypeLayerAndTotimAction(ParamFetcher $paramFetcher, $calculationId, $type, $layer, $totim): Response
+    {
+
+        if (! Uuid::isValid($calculationId)){
+            throw new InvalidUuidException();
+        }
+
+        $result = $this->get('inowas.modflow_projection.calculation_results_finder')
+            ->findValue(
+                ModflowId::fromString($calculationId),
+                CalculationResultType::fromString($type),
+                LayerNumber::fromInteger((int)$layer),
+                TotalTime::fromInt((int)$totim)
+            );
+
+
+        $heatMap = new HeatMap();
+        if ($paramFetcher->get('min') && $paramFetcher->get('max')){
+            $file = $heatMap->createWithAbsoluteLimits($result->data()->toArray(), $paramFetcher->get('min'), $paramFetcher->get('max'));
+        } else {
+            $file = $heatMap->createWithPercentileLimits($result->data()->toArray(), $paramFetcher->get('loper'), $paramFetcher->get('upper'));
+        }
+
+        $response = new Response();
+        $response->headers->set('Content-Type', 'image/png');
+        $response->setContent(file_get_contents($file));
+        return $response;
+    }
 }
