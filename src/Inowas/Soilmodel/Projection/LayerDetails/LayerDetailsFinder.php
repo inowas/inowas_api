1236afe5cc6e27867d49ae3ce39f60c898cbc3d8
Ralf Junghanns <ralf.junghanns@gmail.com>
2017-03-31 14:31:15 +0200

    Update ModflowModelAggregate

---
+++ src/Inowas/Soilmodel/Projection/LayerDetails/LayerDetailsFinder.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ralf
+ * Date: 31.03.17
+ * Time: 14:14
+ */
+
+namespace Inowas\Soilmodel\Projection\LayerDetails;
+
+
+class LayerDetailsFinder
+{
+
+}
