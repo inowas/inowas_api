896a6804daca3a73d5a0bac1b5d4ddeee5684319
Ralf Junghanns <ralf.junghanns@gmail.com>
2017-03-31 23:19:28 +0200

    Add some calculation-projection-stuff

---
+++ src/Inowas/Modflow/Projection/BoundaryList/StressPeriodsProjector.php
@@ -0,0 +1,132 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Inowas\Modflow\Projection\BoundaryList;
+
+use Doctrine\DBAL\Connection;
+use Doctrine\DBAL\Schema\Schema;
+use Inowas\Common\Id\BoundaryId;
+use Inowas\Common\Boundaries\BoundaryName;
+use Inowas\Common\Projection\AbstractDoctrineConnectionProjector;
+use Inowas\Modflow\Model\Event\BoundaryWasAdded;
+use Inowas\Modflow\Model\Event\BoundaryWasAddedToScenario;
+use Inowas\Modflow\Model\Event\BoundaryWasRemoved;
+use Inowas\Modflow\Model\Event\BoundaryWasRemovedFromScenario;
+use Inowas\Modflow\Model\Event\ModflowModelBoundaryWasUpdated;
+use Inowas\Modflow\Model\Event\ModflowScenarioWasAdded;
+use Inowas\Common\Id\ModflowId;
+use Inowas\Modflow\Projection\Table;
+
+class BoundaryListProjector extends AbstractDoctrineConnectionProjector
+{
+
+    public function __construct(Connection $connection) {
+
+        parent::__construct($connection);
+
+        $this->schema = new Schema();
+        $table = $this->schema->createTable(Table::BOUNDARIES);
+        $table->addColumn('id', 'integer', array("unsigned" => true, "autoincrement" => true));
+        $table->addColumn('model_id', 'string', ['length' => 36]);
+        $table->addColumn('boundary_id', 'string', ['length' => 36]);
+        $table->addColumn('type', 'string', ['length' => 255]);
+        $table->addColumn('name', 'string', ['length' => 255]);
+        $table->addColumn('metadata', 'text', ['notnull' => false]);
+        $table->addColumn('geometry', 'text', ['notnull' => false]);
+        $table->addColumn('data', 'text', ['notnull' => false]);
+        $table->setPrimaryKey(['id']);
+        $table->addIndex(array('model_id'));
+    }
+
+    public function onModflowScenarioWasAdded(ModflowScenarioWasAdded $event): void
+    {
+        $sql = sprintf("SELECT * FROM %s WHERE model_id = ?", Table::BOUNDARIES);
+        $stmt = $this->connection->prepare($sql);
+        $stmt->bindValue(1, $event->baseModelId()->toString());
+        $stmt->execute();
+        $boundaries = $stmt->fetchAll();
+
+        foreach ($boundaries as $boundary){
+            $this->insertBoundary(
+                $event->scenarioId(),
+                BoundaryId::fromString($boundary['boundary_id']),
+                BoundaryName::fromString($boundary['name']),
+                $boundary['geometry'],
+                $boundary['type'],
+                $boundary['metadata'],
+                $boundary['data']
+            );
+        }
+    }
+
+    public function onBoundaryWasAdded(BoundaryWasAdded $event): void
+    {
+        $this->insertBoundary(
+            $event->modflowId(),
+            $event->boundary()->boundaryId(),
+            $event->boundary()->name(),
+            $event->boundary()->geometry()->toJson(),
+            $event->boundary()->type(),
+            json_encode($event->boundary()->metadata()),
+            $event->boundary()->dataToJson()
+        );
+    }
+
+    public function onBoundaryWasRemoved(BoundaryWasRemoved $event): void
+    {
+        $this->connection->delete(Table::BOUNDARIES, array(
+            'boundary_id' => $event->boundaryId()->toString()
+        ));
+    }
+
+    public function onModflowModelBoundaryWasUpdated(ModflowModelBoundaryWasUpdated $event): void
+    {
+        $this->connection->delete(Table::BOUNDARIES, array(
+            'boundary_id' => $event->boundary()->boundaryId()->toString(),
+            'model_id' => $event->modflowId()->toString()
+        ));
+    }
+
+    public function onBoundaryWasAddedToScenario(BoundaryWasAddedToScenario $event): void
+    {
+        $this->insertBoundary(
+            $event->scenarioId(),
+            $event->boundary()->boundaryId(),
+            $event->boundary()->name(),
+            $event->boundary()->geometry()->toJson(),
+            $event->boundary()->type(),
+            json_encode($event->boundary()->metadata()),
+            $event->boundary()->dataToJson()
+        );
+    }
+
+    public function onBoundaryWasRemovedFromScenario(BoundaryWasRemovedFromScenario $event): void
+    {
+        $this->connection->delete(Table::BOUNDARIES, array(
+            'boundary_id' => $event->boundaryId()->toString(),
+            'model_id' => $event->scenarioId()->toString(),
+        ));
+    }
+
+    private function insertBoundary(
+        ModflowId $modelId,
+        BoundaryId $boundaryId,
+        BoundaryName $boundaryName,
+        string $boundaryGeometry,
+        string $boundaryType,
+        string $metadata,
+        string $data
+    ): void
+    {
+        $this->connection->insert(Table::BOUNDARIES, array(
+            'model_id' => $modelId->toString(),
+            'boundary_id' => $boundaryId->toString(),
+            'name' => $boundaryName->toString(),
+            'geometry' => $boundaryGeometry,
+            'type' => $boundaryType,
+            'metadata' => $metadata,
+            'data' => $data
+        ));
+    }
+}
