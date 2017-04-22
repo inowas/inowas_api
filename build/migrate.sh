add7f9589e82a5cf6a359e9e4d27d3c2d358964d
Ralf Junghanns <ralf.junghanns@adensis.com>
2017-04-22 21:27:13 +0200

    Add migrate script

---
+++ build/migrate.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+cd $DIR/../
+php bin/console inowas:es:truncate
+php bin/console inowas:projections:reset
+php bin/console inowas:es:migrate 1
