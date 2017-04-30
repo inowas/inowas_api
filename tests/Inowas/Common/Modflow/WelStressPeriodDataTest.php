89ea1f14202e60822808f211c68c563fcfccb7bf
Ralf Junghanns <ralf.junghanns@gmail.com>
2017-04-30 23:07:24 +0200

    Fixes WelStressPeriodDataTest

--- tests/Inowas/Common/Modflow/WelStressPeriodDataTest.php
+++ tests/Inowas/Common/Modflow/WelStressPeriodDataTest.php
@@ -1,10 +1,11 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Tests\Inowas\Common\Modflow;
 
-
+use Inowas\Modflow\Model\Packages\WelStressPeriodData;
 use Inowas\Modflow\Model\Packages\WelStressPeriodGridCellValue;
-use Inowas\Common\Modflow\WelStressPeriodData;
 
 class WelStressPeriodDataTest extends \PHPUnit_Framework_TestCase
 {
@@ -14,7 +15,7 @@ class WelStressPeriodDataTest extends \PHPUnit_Framework_TestCase
         $wellStressperiodData = WelStressPeriodData::create();
         $wellStressperiodData->addGridCellValue(WelStressPeriodGridCellValue::fromParams(0,2,3,4, 7));
         $wellStressperiodData->addGridCellValue(WelStressPeriodGridCellValue::fromParams(1,2,3,4, 5.6));
-        $this->assertEquals('{"stress_periods_data":[[[2,3,4,7]],[[2,3,4,5.6]]]}', \json_encode($wellStressperiodData));
+        $this->assertEquals('{"stress_period_data":{"0":[[2,3,4,7]],"1":[[2,3,4,5.6]]}}', \json_encode($wellStressperiodData));
     }
 
     public function test_add_values_in_the_same_cell_should_add_them(): void
@@ -23,6 +24,6 @@ class WelStressPeriodDataTest extends \PHPUnit_Framework_TestCase
         $wellStressperiodData->addGridCellValue(WelStressPeriodGridCellValue::fromParams(0,2,3,4, 7));
         $wellStressperiodData->addGridCellValue(WelStressPeriodGridCellValue::fromParams(1,2,3,4, 5));
         $wellStressperiodData->addGridCellValue(WelStressPeriodGridCellValue::fromParams(1,2,3,4, 6));
-        $this->assertEquals('{"stress_periods_data":[[[2,3,4,7]],[[2,3,4,11]]]}', \json_encode($wellStressperiodData));
+        $this->assertEquals('{"stress_period_data":{"0":[[2,3,4,7]],"1":[[2,3,4,11]]}}', \json_encode($wellStressperiodData));
     }
 }
