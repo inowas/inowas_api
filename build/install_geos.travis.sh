0e8e8fbd4c6e28d71b6aa13b7d084760bda7f310
Ralf Junghanns <ralf.junghanns@gmail.com>
2017-05-04 15:26:27 +0200

    Update Travis-Config

--- build/install_geos.travis.sh
+++ build/install_geos.travis.sh
@@ -1,1 +1,27 @@
 #!/usr/bin/env bash
+
+sudo apt-get update
+sudo apt-get remove 'libgeos.*'
+sudo apt-get autoremove
+
+VERSION=3.4.2
+wget https://github.com/libgeos/libgeos/archive/$VERSION.tar.gz
+tar zxf $VERSION.tar.gz
+cd libgeos-$VERSION
+./autogen.sh
+./configure --prefix=/usr
+make
+sudo make install
+cd ..
+
+wget https://github.com/libgeos/php-geos/archive/1.0.0.tar.gz
+tar zxf 1.0.0.tar.gz
+cd php-geos-1.0.0
+./autogen.sh
+./configure
+make
+mv modules/geos.so $(php-config --extension-dir)
+cd ..
+
+echo "extension=geos.so" > geos.ini
+phpenv config-add geos.ini
