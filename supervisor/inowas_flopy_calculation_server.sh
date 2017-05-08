d0c8a19f98af39a30e8a28a6bddb9ac46ba2d59a
Ralf Junghanns <ralf.junghanns@gmail.com>
2017-05-08 10:27:30 +0200

    Set data-folder with supervisor-install

--- supervisor/inowas_flopy_calculation_server.sh
+++ supervisor/inowas_flopy_calculation_server.sh
@@ -2,4 +2,4 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-/usr/bin/python3 -u $DIR/../py/pyModelling/inowas.flopy.calculation.server.py /tmp/data
+/usr/bin/python3 -u $DIR/../py/pyModelling/inowas.flopy.calculation.server.py $1
--- supervisor/inowas_flopy_readdata_rpc_server.sh
+++ supervisor/inowas_flopy_readdata_rpc_server.sh
@@ -2,4 +2,4 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-/usr/bin/python3 -u $DIR/../py/pyModelling/inowas.flopy.read_data.rpc.server.py /tmp/data
+/usr/bin/python3 -u $DIR/../py/pyModelling/inowas.flopy.read_data.rpc.server.py $1
