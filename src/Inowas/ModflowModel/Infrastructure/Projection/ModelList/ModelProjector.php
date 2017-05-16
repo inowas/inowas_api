64af5933ea277ace7ec8a5c894f43ec919251a4d
Ralf Junghanns <ralf.junghanns@gmail.com>
2017-05-16 22:24:33 +0200

    s

--- src/Inowas/ModflowModel/Infrastructure/Projection/ModelList/ModelProjector.php
+++ src/Inowas/ModflowModel/Infrastructure/Projection/ModelList/ModelProjector.php
@@ -9,6 +9,7 @@ use Doctrine\DBAL\Schema\Schema;
 use Doctrine\ORM\EntityManager;
 use Inowas\AppBundle\Model\User;
 use Inowas\Common\Projection\AbstractDoctrineConnectionProjector;
+use Inowas\Common\Soilmodel\SoilmodelId;
 use Inowas\ModflowModel\Model\Event\AreaGeometryWasUpdated;
 use Inowas\ModflowModel\Model\Event\BoundingBoxWasChanged;
 use Inowas\ModflowModel\Model\Event\DescriptionWasChanged;
@@ -17,6 +18,7 @@ use Inowas\ModflowModel\Model\Event\ModflowModelWasCloned;
 use Inowas\ModflowModel\Model\Event\NameWasChanged;
 use Inowas\ModflowModel\Model\Event\ModflowModelWasCreated;
 use Inowas\ModflowModel\Infrastructure\Projection\Table;
+use Inowas\ModflowModel\Model\Event\SoilModelIdWasChanged;
 
 class ModelProjector extends AbstractDoctrineConnectionProjector
 {
@@ -33,6 +35,7 @@ class ModelProjector extends AbstractDoctrineConnectionProjector
         $table->addColumn('id', 'integer', array("unsigned" => true, "autoincrement" => true));
         $table->addColumn('model_id', 'string', ['length' => 36]);
         $table->addColumn('user_id', 'string', ['length' => 36]);
+        $table->addColumn('soilmodel_id', 'string', ['length' => 36, 'notnull' => false]);
         $table->addColumn('user_name', 'string', ['length' => 255]);
         $table->addColumn('name', 'string', ['length' => 255]);
         $table->addColumn('description', 'string', ['length' => 255]);
@@ -45,11 +48,47 @@ class ModelProjector extends AbstractDoctrineConnectionProjector
         $table->addIndex(array('model_id'));
     }
 
+    public function onAreaGeometryWasUpdated(AreaGeometryWasUpdated $event): void
+    {
+        $this->connection->update(Table::MODEL_DETAILS, array(
+            'area' => $event->geometry()->toJson(),
+        ),
+            array('model_id' => $event->modelId()->toString())
+        );
+    }
+
+    public function onBoundingBoxWasChanged(BoundingBoxWasChanged $event): void
+    {
+        $this->connection->update(Table::MODEL_DETAILS, array(
+            'bounding_box' => json_encode($event->boundingBox()),
+        ),
+            array('model_id' => $event->modflowId()->toString())
+        );
+    }
+
+    public function onDescriptionWasChanged(DescriptionWasChanged $event): void
+    {
+        $this->connection->update(Table::MODEL_DETAILS,
+            array('description' => $event->description()->toString()),
+            array('model_id' => $event->modflowModelId()->toString())
+        );
+    }
+
+    public function onGridSizeWasChanged(GridSizeWasChanged $event): void
+    {
+        $this->connection->update(Table::MODEL_DETAILS, array(
+            'grid_size' => json_encode($event->gridSize())
+        ),
+            array('model_id' => $event->modflowId()->toString())
+        );
+    }
+
     public function onModflowModelWasCreated(ModflowModelWasCreated $event): void
     {
         $this->connection->insert(Table::MODEL_DETAILS, array(
             'model_id' => $event->modelId()->toString(),
             'user_id' => $event->userId()->toString(),
+            'soilmodel_id' => SoilmodelId::generate(),
             'user_name' => $this->getUserNameByUserId($event->userId()->toString()),
             'name' => '',
             'description' => '',
@@ -65,7 +104,7 @@ class ModelProjector extends AbstractDoctrineConnectionProjector
     {
 
         $rows = $this->connection->fetchAll(
-            sprintf('SELECT name as name, description, area, grid_size, bounding_box FROM %s WHERE model_id = :model_id', Table::MODEL_DETAILS),
+            sprintf('SELECT * FROM %s WHERE model_id = :model_id', Table::MODEL_DETAILS),
             ['model_id' => $event->baseModelId()->toString()]
         );
 
@@ -85,15 +124,6 @@ class ModelProjector extends AbstractDoctrineConnectionProjector
         }
     }
 
-    public function onAreaGeometryWasUpdated(AreaGeometryWasUpdated $event): void
-    {
-        $this->connection->update(Table::MODEL_DETAILS, array(
-            'area' => $event->geometry()->toJson(),
-        ),
-            array('model_id' => $event->modelId()->toString())
-        );
-    }
-
     public function onNameWasChanged(NameWasChanged $event): void
     {
         $this->connection->update(Table::MODEL_DETAILS,
@@ -102,29 +132,11 @@ class ModelProjector extends AbstractDoctrineConnectionProjector
         );
     }
 
-    public function onDescriptionWasChanged(DescriptionWasChanged $event): void
+    public function onSoilModelIdWasChanged(SoilModelIdWasChanged $event): void
     {
         $this->connection->update(Table::MODEL_DETAILS,
-            array('description' => $event->description()->toString()),
+            array('soilmodel_id' => $event->soilModelId()->toString()),
             array('model_id' => $event->modflowModelId()->toString())
-        );
-    }
-
-    public function onBoundingBoxWasChanged(BoundingBoxWasChanged $event): void
-    {
-        $this->connection->update(Table::MODEL_DETAILS, array(
-            'bounding_box' => json_encode($event->boundingBox()),
-        ),
-            array('model_id' => $event->modflowId()->toString())
-        );
-    }
-
-    public function onGridSizeWasChanged(GridSizeWasChanged $event): void
-    {
-        $this->connection->update(Table::MODEL_DETAILS, array(
-            'grid_size' => json_encode($event->gridSize())
-        ),
-            array('model_id' => $event->modflowId()->toString())
         );
     }
 
