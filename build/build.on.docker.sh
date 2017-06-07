9ad0ccba9c447d053ecbb36776957f87c176abf9
Ralf Junghanns <ralf.junghanns@gmail.com>
2017-06-07 12:40:30 +0200

    Update script to set folderpermissions

--- build/build.on.docker.sh
+++ build/build.on.docker.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 # Set folder permissions
+rm -rf var/cache/*
+rm -rf var/logs/*
 HTTPDUSER=`ps axo user,comm | grep -E '[a]pache|[h]ttpd|[_]www|[w]ww-data|[n]ginx' | grep -v root | head -1 | cut -d\  -f1`
 setfacl -R -m u:"$HTTPDUSER":rwX -m u:`whoami`:rwX var
 setfacl -dR -m u:"$HTTPDUSER":rwX -m u:`whoami`:rwX var
