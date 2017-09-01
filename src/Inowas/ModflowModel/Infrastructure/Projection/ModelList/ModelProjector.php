ea80732a41ab77aeb307a43b31cf92c51601e1c0
Ralf Junghanns <ralf.junghanns@gmail.com>
2017-09-01 14:18:41 +0200

    Merge branch 'dev' of github.com:inowas/inowas into dev
    # Conflicts:
    #	src/Inowas/ModflowModel/Infrastructure/Projection/ModelList/ModelProjector.php
    #	src/Inowas/ModflowModel/Model/Handler/RequestModflowModelCalculationHandler.php

--- src/Inowas/ModflowModel/Infrastructure/Projection/ModelList/ModelProjector.php
+++ src/Inowas/ModflowModel/Infrastructure/Projection/ModelList/ModelProjector.php
@@ -19,7 +19,6 @@ use Inowas\ModflowModel\Model\Event\BoundaryWasUpdated;
 use Inowas\ModflowModel\Model\Event\BoundingBoxWasChanged;
 use Inowas\ModflowModel\Model\Event\CalculationIdWasChanged;
 use Inowas\ModflowModel\Model\Event\CalculationWasRequested;
-use Inowas\ModflowModel\Model\Event\CalculationWasRequested;
 use Inowas\ModflowModel\Model\Event\DescriptionWasChanged;
 use Inowas\ModflowModel\Model\Event\GridSizeWasChanged;
 use Inowas\ModflowModel\Model\Event\LengthUnitWasUpdated;
