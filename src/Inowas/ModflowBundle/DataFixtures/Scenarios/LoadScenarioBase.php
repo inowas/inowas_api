42674a758e35caf2e40222a4119a6194951de0b2
Ralf Junghanns <ralf.junghanns@gmail.com>
2017-09-04 10:05:20 +0200

    Add summer-school users

--- src/Inowas/ModflowBundle/DataFixtures/Scenarios/LoadScenarioBase.php
+++ src/Inowas/ModflowBundle/DataFixtures/Scenarios/LoadScenarioBase.php
@@ -45,6 +45,31 @@ abstract class LoadScenarioBase implements ContainerAwareInterface, DataFixtureI
             array('catalin.stefan', 'Catalin Stefan', 'catalin.stefan@tu-dresden.de', '#inowas#'),
             array('martin.wudenka', 'Martin Wudenka', 'martin.wudenka@tu-dresden.de', '#inowas#'),
             array('nawapi', 'nawapi', 'nawapi@inowas.com', 'na-ql-ww-pi'),
+            array('inowas1', 'inowas1@gast', 'cheekohk'),
+            array('malena', 'malena@gast', 'wohchoad'),
+            array('jorge', 'jorge@gast', 'geuwaexu'),
+            array('ahmad', 'ahmad@gast', 'eipobung'),
+            array('jonas', 'jonas@gast', 'eyipuamo'),
+            array('guzman', 'guzman@gast', 'xookahbi'),
+            array('cristiano', 'cristiano@gast', 'ohfohfum'),
+            array('anna', 'anna@gast', 'ahmohnah'),
+            array('byambasuren', 'byambasuren@gast', 'aejohque'),
+            array('jeanne', 'jeanne@gast', 'xiegooge'),
+            array('nguyen', 'nguyen@gast', 'uvupauho'),
+            array('meng', 'meng@gast', 'thauzeej'),
+            array('xueyan', 'xueyan@gast', 'rohpooqu'),
+            array('badia', 'badia@gast', 'caegouno'),
+            array('rodrigo', 'rodrigo@gast', 'ahdeogei'),
+            array('guillermo', 'guillermo@gast', 'yeuvoolo'),
+            array('angel', 'angel@gast', 'aezepees'),
+            array('sospeter', 'sospeter@gast', 'uuchahth'),
+            array('bhesh', 'bhesh@gast', 'teigotab'),
+            array('vahid', 'vahid@gast', 'ophiebah'),
+            array('andre', 'andre@gast', 'ohsohshi'),
+            array('adeyinka', 'adeyinka@gast', 'aimeipae'),
+            array('muna', 'muna@gast', 'iegheith'),
+            array('bewuket', 'bewuket@gast', 'hohreith'),
+            array('mutaz', 'mutaz@gast', 'chohexai')
         );
 
         foreach ($userList as $item){
@@ -70,40 +95,12 @@ abstract class LoadScenarioBase implements ContainerAwareInterface, DataFixtureI
         $this->ownerId = $owner->getId()->toString();
     }
 
-    protected function loadHeadsFromFile($filename, $invert = false){
-
-        if (!file_exists($filename) || !is_readable($filename)) {
-            echo "File not found.\r\n";
-            return FALSE;
-        }
-
-        $headsJSON = file_get_contents($filename, true);
-        $heads = json_decode($headsJSON, true);
-
-
-        for ($iy = 0; $iy < count($heads); $iy++){
-            for ($ix = 0; $ix < count($heads[0]); $ix++){
-                if (abs($heads[$iy][$ix]) > 9999){
-                    $heads[$iy][$ix] = null;
-                } else {
-                    $heads[$iy][$ix] = round($heads[$iy][$ix], 3);
-
-                    if ($invert) {
-                        $heads[$iy][$ix] = -$heads[$iy][$ix];
-                    }
-                }
-            }
-        }
-
-        return $heads;
-    }
-
     protected function loadRowsFromCsv($filename): array {
         $header = null;
         $rows = array();
-        if (($handle = fopen($filename, "r")) !== FALSE) {
-            while (($data = fgetcsv($handle, 1000, ";")) !== FALSE) {
-                if ($header == null){
+        if (($handle = fopen($filename, 'rb')) !== FALSE) {
+            while (($data = fgetcsv($handle, 1000, ';')) !== FALSE) {
+                if (null === $header) {
                     $header = $data;
                     continue;
                 }
@@ -120,7 +117,7 @@ abstract class LoadScenarioBase implements ContainerAwareInterface, DataFixtureI
     protected function loadHeaderFromCsv($filename): array
     {
         $data = array();
-        if (($handle = fopen($filename, "r")) !== FALSE) {
+        if (($handle = fopen($filename, 'rb')) !== FALSE) {
             $data = fgetcsv($handle, 1000, ";");
             fclose($handle);
         }
@@ -131,7 +128,7 @@ abstract class LoadScenarioBase implements ContainerAwareInterface, DataFixtureI
     protected function getDates(array $header): array{
         $dates = array();
         foreach ($header as $data){
-            if (explode(':', $data)[0] == 'date'){
+            if (explode(':', $data)[0] === 'date'){
                 $dates[] = $data;
             }
         }
