ccb3c66bc82f4fe5346d81b493ecc9aef061619f
Ralf Junghanns <ralf.junghanns@gmail.com>
2017-09-05 10:06:41 +0200

    Bugfix in Listener

--- src/Inowas/ModflowModel/Infrastructure/EventListener/CalculationWasStartedListener.php
+++ src/Inowas/ModflowModel/Infrastructure/EventListener/CalculationWasStartedListener.php
@@ -7,6 +7,7 @@ use Inowas\ModflowModel\Model\Event\CalculationWasStarted;
 use Inowas\ModflowModel\Service\AMQPFlopyCalculation;
 use Inowas\ModflowModel\Service\AMQPModflowCalculation;
 use Inowas\ModflowModel\Service\ModflowPackagesManager;
+use Prooph\Common\Messaging\DomainEvent;
 
 class CalculationWasStartedListener
 {
@@ -30,5 +31,23 @@ class CalculationWasStartedListener
         $packages = $this->packagesManager->getPackages($event->calculationId());
         $request = FlopyCalculationRequest::fromParams($event->modelId(), $event->calculationId(), $packages);
         $this->calculator->calculate($request);
+    }
+
+    public function onEvent(DomainEvent $e): void
+    {
+        $handler = $this->determineEventMethodFor($e);
+        if (! method_exists($this, $handler)) {
+            throw new \RuntimeException(sprintf(
+                'Missing event method %s for projector %s',
+                $handler,
+                get_class($this)
+            ));
+        }
+        $this->{$handler}($e);
+    }
+
+    protected function determineEventMethodFor(DomainEvent $e)
+    {
+        return 'on' . implode(array_slice(explode('\\', get_class($e)), -1));
     }
 }
