e7ff3e82b5db358dc2b2ca52b3d1592d9149fa9c
Ralf Junghanns <ralf.junghanns@gmail.com>
2017-09-08 10:27:01 +0200

    Update Datafixtures for summerschool

--- src/Inowas/ModflowBundle/DataFixtures/Scenarios/RioPrimero/RioPrimeroArea.php
+++ src/Inowas/ModflowBundle/DataFixtures/Scenarios/RioPrimero/RioPrimeroArea.php
@@ -1,73 +1,17 @@
 <?php
 
 namespace Inowas\ModflowBundle\DataFixtures\Scenarios\RioPrimero;
-
-use Inowas\Common\Boundaries\ConstantHeadBoundary;
-use Inowas\Common\Boundaries\ConstantHeadDateTimeValue;
-use Inowas\Common\Boundaries\Metadata;
-use Inowas\Common\Boundaries\BoundaryType;
-use Inowas\Common\Boundaries\GeneralHeadBoundary;
-use Inowas\Common\Boundaries\GeneralHeadDateTimeValue;
-use Inowas\Common\Boundaries\ObservationPoint;
-use Inowas\Common\Boundaries\RechargeBoundary;
-use Inowas\Common\Boundaries\RechargeDateTimeValue;
-use Inowas\Common\Boundaries\RiverBoundary;
-use Inowas\Common\Boundaries\RiverDateTimeValue;
-use Inowas\Common\Boundaries\WellBoundary;
-use Inowas\Common\Boundaries\WellDateTimeValue;
-use Inowas\Common\Boundaries\WellType;
-use Inowas\Common\DateTime\DateTime;
 use Inowas\Common\Geometry\Geometry;
-use Inowas\Common\Geometry\LineString;
-use Inowas\Common\Geometry\Point;
 use Inowas\Common\Geometry\Polygon;
-use Inowas\Common\Grid\AffectedLayers;
-use Inowas\Common\Grid\BoundingBox;
 use Inowas\Common\Grid\GridSize;
-use Inowas\Common\Grid\LayerNumber;
 use Inowas\Common\Id\ModflowId;
-use Inowas\Common\Id\ObservationPointId;
 use Inowas\Common\Id\UserId;
-use Inowas\Common\Interpolation\PointValue;
-use Inowas\Common\Modflow\Botm;
-use Inowas\Common\Modflow\Hani;
-use Inowas\Common\Modflow\Hk;
-use Inowas\Common\Modflow\Layavg;
-use Inowas\Common\Modflow\Laytyp;
-use Inowas\Common\Modflow\Laywet;
 use Inowas\Common\Modflow\LengthUnit;
 use Inowas\Common\Modflow\Description;
 use Inowas\Common\Modflow\Name;
-use Inowas\Common\Modflow\Ss;
-use Inowas\Common\Modflow\Sy;
-use Inowas\Common\Modflow\Top;
-use Inowas\Common\Modflow\Vka;
-use Inowas\Common\Soilmodel\Layer;
-use Inowas\Common\Soilmodel\LayerId;
-use Inowas\ModflowModel\Model\Command\AddBoundary;
-use Inowas\ModflowModel\Model\Command\AddLayer;
-use Inowas\ModflowModel\Model\Command\ChangeDescription;
-use Inowas\ModflowModel\Model\Command\ChangeName;
-use Inowas\ModflowModel\Model\Packages\OcStressPeriod;
-use Inowas\ModflowModel\Model\Packages\OcStressPeriodData;
-use Inowas\Common\Modflow\PackageName;
-use Inowas\Common\Modflow\ParameterName;
-use Inowas\Common\Modflow\StressPeriod;
-use Inowas\Common\Modflow\StressPeriods;
 use Inowas\Common\Modflow\TimeUnit;
-use Inowas\ModflowModel\Model\Command\CalculateModflowModel;
-use Inowas\ModflowModel\Model\Command\ChangeBoundingBox;
-use Inowas\ModflowModel\Model\Command\ChangeFlowPackage;
 use Inowas\ModflowModel\Model\Command\CreateModflowModel;
 use Inowas\ModflowBundle\DataFixtures\Scenarios\LoadScenarioBase;
-use Inowas\ModflowModel\Model\Command\UpdateModflowPackageParameter;
-use Inowas\ModflowModel\Model\Command\UpdateStressPeriods;
-use Inowas\ScenarioAnalysis\Model\Command\CreateScenario;
-use Inowas\ScenarioAnalysis\Model\Command\CreateScenarioAnalysis;
-use Inowas\ScenarioAnalysis\Model\ScenarioAnalysisDescription;
-use Inowas\ScenarioAnalysis\Model\ScenarioAnalysisId;
-use Inowas\ScenarioAnalysis\Model\ScenarioAnalysisName;
-use Inowas\Soilmodel\Model\LayerInterpolationConfiguration;
 
 class RioPrimeroArea extends LoadScenarioBase
 {
@@ -92,8 +36,8 @@ class RioPrimeroArea extends LoadScenarioBase
         $commandBus->dispatch(CreateModflowModel::newWithAllParams(
             $ownerId,
             $baseModelId,
-            Name::fromString('BaseModel Rio Primero 2015'),
-            Description::fromString('BaseModel Rio Primero 2015'),
+            Name::fromString('Rio Primero Model Area'),
+            Description::fromString('Rio Primero Model Area'),
             $polygon,
             $gridSize,
             $boundingBox,
