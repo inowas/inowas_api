5a229b77ca7073aed13a7f0794a5cbb2b6cef1d3
Ralf Junghanns <ralf.junghanns@gmail.com>
2018-08-31 18:47:38 +0200

    Update Infrastructure

--- build/setFolderpermissions.sh
+++ build/setFolderpermissions.sh
@@ -4,5 +4,5 @@ rm -rf var/cache/*
 rm -rf var/logs/*
 
 HTTPDUSER=`ps axo user,comm | grep -E '[a]pache|[h]ttpd|[_]www|[w]ww-data|[n]ginx' | grep -v root | head -1 | cut -d\  -f1`
-sudo setfacl -R -m u:"$HTTPDUSER":rwX -m u:`whoami`:rwX var
-sudo setfacl -dR -m u:"$HTTPDUSER":rwX -m u:`whoami`:rwX var
+setfacl -R -m u:"$HTTPDUSER":rwX -m u:`whoami`:rwX var
+setfacl -dR -m u:"$HTTPDUSER":rwX -m u:`whoami`:rwX var
