4e281a8bec38ca0d011f8a6a54d7beea7e30d14d
Ralf Junghanns <ralf.junghanns@gmail.com>
2018-10-04 14:17:48 +0200

    Update build-scripts

--- build/setDataFolderPermissions.sh
+++ build/setDataFolderPermissions.sh
@@ -6,6 +6,10 @@ ARGS=("$@")
 FOLDER=${ARGS[0]}
 HTTPDUSER=`ps axo user,comm | grep -E '[a]pache|[h]ttpd|[_]www|[w]ww-data|[n]ginx' | grep -v root | head -1 | cut -d\  -f1`
 
+echo "Set permissions for user ${HTTPDUSER} at folder ${FOLDER}"
 chown ${HTTPDUSER}:${HTTPDUSER} ${FOLDER} -R
 setfacl -R -m u:"$HTTPDUSER":rwX -m u:`whoami`:rwX ${FOLDER}
 setfacl -dR -m u:"$HTTPDUSER":rwX -m u:`whoami`:rwX ${FOLDER}
+
+echo "You're running php version:"
+php -v
